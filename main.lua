027721877352c2b1da2e6a9974c555f7a7524854
Lbelus <lorris.belus@icloud.com>
2024-08-09 01:37:40 +0200

    [lbs] add particle descriptor

--- main.lua
+++ main.lua
@@ -1,45 +1,79 @@
-PARTICLE = {}
+MODIFIER = {}
 
-function PARTICLE:new(x, y, life, angle, velocity, color)
-	local pcle
-	pcle.position = {
+function MODIFIER:new()
+	local mod = {}
+	setmetatable(mod, self)
+	self.__index = self
+	return mod
+end
+
+PARTICLE_DESCRIPTOR = {}
+
+function PARTICLE_DESCRIPTOR:new(x, y, life, angle, velocity, color)
+	local pd = {}
+	pd.position = {
 		x = x,
 		y = y
 	}
-	pcle.x = x
-	pcle.y = y
-	pcle.life = life
-	local angle_radian = angle * math.pi / 180
+	pd.life = life
+	pd.angle = angle
+	pd.velocity = velocity
+	pd.color = color
+	setmetatable(pd, self)
+	self.__index = self
+	return pd
+end
+
+
+PARTICLE = MODIFIER:new()
+
+function PARTICLE:new(pd)
+	local pcle = {}
+	pcle.position = pd.position
+	pcle.life = pd.life
+	local angle_radian = pd.angle * math.pi / 180
 	pcle.velocity = {
-		x = velocity * math.cos(angle_radian),
-		y = -velocity * math.sin(angle_radian)
+		x = pd.velocity * math.cos(angle_radian),
+		y = -pd.velocity * math.sin(angle_radian)
 	}
-	pcle.color = color
+	pcle.color = pd.color
 	setmetatable(pcle, self)
 	self.__index = self
 	return pcle
 end
 
-
 function PARTICLE:update(delta)
 	self.life = self.life - delta
+	self.position.x = self.position.x + self.velocity.x * delta
+	self.position.y = self.position.y + self.velocity.y * delta
+end
 
-	self.position.x = self.position.x + self.velocity * delta
-	self.position.y = self.position.y + self.velocity * delta
+function PARTICLE:print_pos()
+	print("Particle position - x: " .. self.position.x .. ", y: " .. self.position.y)
 end
 
 
 
--- function love.draw()
---    love.graphics.print("Hello World", 400, 300)
--- end
+EMITER = {}
+
+function EMITER:new()
+	local emiter = {}
+	setmetatable(emiter, self)
+	self.__index = self
+	return emiter
+end
+
+function EMITER:emit()
 
 
 function main()
-	local blue_spark = PARTICLE:new(1, 1, 60, 10, 'blue')
-	print(blue_spark)
+	print("begin particle test")
+	local blue_spark = PARTICLE:new(1, 1, 60, 10, 100, 'blue')  -- Add a velocity value
 	for index = 1, 10 do
-	
+		blue_spark:update(1)  -- Simulate updating with delta time of 1
+		blue_spark:print_pos()
 	end
-
 end
+
+
+main()
