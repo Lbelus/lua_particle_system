8624cbb3ba80029aaa57e42d3819dacba0d0fef0
Lbelus <lorris.belus@icloud.com>
2024-08-19 03:19:54 +0200

    add renderer

--- main.lua
+++ main.lua
@@ -16,6 +16,7 @@ function PARTICLE_DESCRIPTOR:new(x, y, life, angle, velocity, color)
 		y = y
 	}
 	pd.life = life
+	pd.lifecycle = 0
 	pd.angle = angle
 	pd.velocity = velocity
 	pd.color = color
@@ -41,21 +42,29 @@ function PARTICLE:new(pd)
 	return pcle
 end
 
-function PARTICLE:update(delta)
+function PARTICLE:update(delta, pixel_per_frame)
 	self.life = self.life - delta
-	self.position.x = self.position.x + self.velocity.x * delta
-	self.position.y = self.position.y + self.velocity.y * delta
+	self.position.x = self.position.x + self.velocity.x * (delta * pixel_per_frame)
+	self.position.y = self.position.y + self.velocity.y * (delta * pixel_per_frame)
 end
 
 function PARTICLE:print_pos()
 	print("Particle position - x: " .. self.position.x .. ", y: " .. self.position.y)
 end
 
+function PARTICLE:draw()
+	love.graphics.setColor(self.color)
+	love.graphics.circle("fill", self.position.x, self.position.y, 5)
+end
+
+
 EMITTER_CONFIG = {}
 
-function EMITTER_CONFIG:new(total_particles)
+function EMITTER_CONFIG:new(total_particles, pixel_per_frame)
 	local config = {}
 	config.total_particles = total_particles
+	config.pixel_per_frame = pixel_per_frame
+	config.spacing = spacing
 	setmetatable(config, self)
 	self.__index = self
 	return config
@@ -83,26 +92,79 @@ function EMITTER:emit(particle)
 	table.insert(self.particle_pool, particle)
 end
 
-function EMITTER:update(delta)
-	local particle = table.remove(self.particle_pool)
-	if particle then
-		particle:update(delta)
-		particle:print_pos()
-		if particle.life > 0 then
-			self:emit(particle)
+local function isEmpty(table)
+    return next(table) == nil
+end
+
+-- not the spacing that i want
+function EMITTER:update(delta, emit_count)
+	local size =  math.min(emit_count, #self.particle_pool)
+	local particle_stack = {}
+	for index = 1, size do
+		table.insert(particle_stack, table.remove(self.particle_pool))
+	end
+	local delay = 1 / #particle_stack
+	while not isEmpty(particle_stack) do
+		local particle = table.remove(particle_stack)
+		if particle then
+			particle:update((delta*delay), self.config.pixel_per_frame)
+			if particle.life > 0 then
+				self:emit(particle)
+			end
 		end
+		delay = delay + delay
+	end 
+end
+
+-- // delay effect
+-- function EMITTER:update(delta, emit_count)
+-- 	local size =  math.min(emit_count, #self.particle_pool)
+-- 	local particle_stack = {}
+-- 	for index = 1, size do
+-- 		table.insert(particle_stack, table.remove(self.particle_pool))
+-- 	end
+-- 	local delay = 1 / #particle_stack
+-- 	while not isEmpty(particle_stack) do
+-- 		local particle = table.remove(particle_stack)
+-- 		if particle then
+-- 			particle:update((delta * delay), self.config.pixel_per_frame)
+-- 			if particle.life > 0 then
+-- 				self:emit(particle)
+-- 			end
+-- 		end
+-- 		delay = delay + delay
+-- 	end
+-- end
+
+
+function EMITTER:draw()
+	for _, particle in ipairs(self.particle_pool) do
+		particle:draw()
 	end
 end
 
-function main()
-	print("begin emitter test")
-	local config = EMITTER_CONFIG:new(10)
-	local blue_spark_pd = PARTICLE_DESCRIPTOR:new(1, 1, 2, 60, 10, 'blue')
-	local blue_spark_emitter = EMITTER:new(config, blue_spark_pd)
-	for i = 1, 10 do
-		blue_spark_emitter:update(1)
-	end
+-- function main()
+-- 	print("begin emitter test")
+-- 	local config = EMITTER_CONFIG:new(10)
+-- 	local blue_spark_pd = PARTICLE_DESCRIPTOR:new(1, 1, 2, 60, 10, 'blue')
+-- 	local blue_spark_emitter = EMITTER:new(config, blue_spark_pd)
+-- 	for i = 1, 10 do
+-- 		blue_spark_emitter:update(1)
+-- 	end
+-- end
+
+-- main()
+
+function love.load()
+	local config = EMITTER_CONFIG:new(100, 1, 1)
+	local blue_spark_pd = PARTICLE_DESCRIPTOR:new(400, 300, 5, 60, 100, {0, 0, 1})  -- Color as RGB values (0-1 range)
+	blue_spark_emitter = EMITTER:new(config, blue_spark_pd)
 end
 
-main()
+function love.update(dt)
+	blue_spark_emitter:update(dt, 5)
+end
 
+function love.draw()
+	blue_spark_emitter:draw()
+end
